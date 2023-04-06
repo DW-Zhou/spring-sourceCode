82e469d2f746d8f6a3945ff356f06d4c429ccecf
北斗 <dengzhou@cai-inc.com>
2023-04-06 22:21:00 +0800

    first commit

---
+++ src/test/java/com/bd/springsourcecode/SpringSourceCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd.springsourcecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSourceCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
